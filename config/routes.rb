fa309306a77a521d3cf3ab593b65ce6bd8b12426
kitliew21 <kitliew21@gmail.com>
2016-09-17 22:00:09 +0100

    major update on css/navbar

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
+  get 'static_pages/landing_page'
+
 	root 'static_pages#landing_page'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
