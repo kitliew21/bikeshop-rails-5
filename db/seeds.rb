7c026200972412d1f48c34c594c6c4681c16b77f
kitliew21 <kitliew21@gmail.com>
2016-09-17 20:59:30 +0100

    4.2 seed and migrate database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.create(:name => 'Apollo Star Bike', :description => 'White and cool green frame.', :image_url => 'http://i1.adis.ws/i/washford/653869?$pd_main_v2$', :price => '100')
+
+Product.create(:name => 'Apollo Phaze Bike', :description => 'Grey frame with 18 speed Shimano gears.', :image_url => 'http://i1.adis.ws/i/washford/159552?$pd_main_v2$', :price => '180')
+
+Product.create(:name => 'Apollo Twilight Bike', :description => 'Black frame with every features for a safe ride', :image_url => 'http://i1.adis.ws/i/washford/686141?$pd_main_v2$', :price => '120')
+
+Product.create(:name => 'VooDoo Limba Bike', :description => 'Double butted blue aluminium frame.', :image_url => 'http://i1.adis.ws/i/washford/150593?$pd_main_v2$', :price => '240')
+
+Product.create(:name => 'Carrera Virtuoso Bike', :description => 'White lightweight aluminium frame.', :image_url => 'http://i1.adis.ws/i/washford/156715?$pd_main_v2$', :price => '650')
+
+Product.create(:name => 'Apollo Slant Bike', :description => 'Red frame with 14 speed Shimano gears ', :image_url => 'http://i1.adis.ws/i/washford/159453?$pd_main_v2$', :price => '480')
