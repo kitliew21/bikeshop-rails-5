d613a8eb11f2599bb276fceffe0a7033590eaa80
kitliew21 <kitliew21@gmail.com>
2016-09-18 16:31:01 +0100

    5.3 mailers heroku part 1

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
 
+  post 'static_pages/thank_you'
+
   get '/products/:id', to: 'products#show'
 
   get 'static_pages/about'
@@ -12,7 +14,7 @@ Rails.application.routes.draw do
 
   get 'static_pages/landing_page'
 
-	root 'static_pages#index'
+	root 'static_pages#landing_page'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
