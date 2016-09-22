878005af49ff8082a09f680c6bf48d704bcec296
kitliew21 <kitliew21@gmail.com>
2016-09-22 03:40:47 +0100

    5.7 comments

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,46 +3,18 @@ class OrdersController < ApplicationController
 before_filter :authenticate_user!
 
   def index
-    @orders = Order.all
   end
 
   def show
   end
 
   def new
-    @order = Order.new
   end
 
   def create
-    @order = Order.new(order_params)
-
-    respond_to do |format|
-      if @order.save
-        format.html { redirect_to @order, notice: 'Order was successfully created.' }
-        format.json { render :show, status: :created, location: @order }
-      else
-        format.html { render :new }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   def destroy
-    @order.destroy
-    respond_to do |format|
-      format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_order
-      @order = Order.find(params[:id])
-    end
-
-    def order_params
-      params.require(:order).permit(:name, :description, :image_url, :price)
-    end
-  end
+end
