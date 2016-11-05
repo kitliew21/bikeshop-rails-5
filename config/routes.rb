e0c6a07eca4bee4f55a0539326d035387b46fa94
kitliew21 <kitliew21@gmail.com>
2016-11-05 03:31:55 +0000

    style upgrade

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
 
   get 'static_pages/landing_page'
 
-	root 'static_pages#landing_page'
+	root 'static_pages#index'
 
   resources :orders, only: [:index, :show, :create, :destroy]
 
