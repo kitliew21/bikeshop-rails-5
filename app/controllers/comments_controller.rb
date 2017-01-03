5dc0d39336a8fded4741198a4ca39af0921436d7
kitliew21 <kitliew21@gmail.com>
2017-01-03 04:27:45 +0000

    Update: fix rails server

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,15 +5,16 @@ class CommentsController < ApplicationController
     @user = current_user
     @comment = @product.comments.new(comment_params)
     @comment.user = current_user
-  respond_to do |format|
-    if @comment.save
-      # ActionCable.server.broadcast 'product_channel', comment: @comment, average_rating: @comment.product.average_rating
-      format.html { redirect_to @product, notice: 'Review was created successfully.' }
-      format.json { render :show, status: :created, location: @product }
-      format.js
-    else
-      format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
-      format.json { render json: @comment.errors, status: :unprocessable_entity }
+    respond_to do |format|
+      if @comment.save
+        # ActionCable.server.broadcast 'product_channel', comment: @comment, average_rating: @comment.product.average_rating
+        format.html { redirect_to @product, notice: 'Review was created successfully.' }
+        format.json { render :show, status: :created, location: @product }
+        format.js
+      else
+        format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
     end
   end
 
